f09e633872805c2424c5b1f41e137db7dc6f37e6
Ramon Moraes <ramon@vyscond.io>
2020-04-04 17:05:11 -0300

    Updating setup
    - setup.py now sources version from the main module
    - setup.py now includes the glade templates properly
    - ui.py now holds the the GTK logic and loads glade templates properly
    - Added few necessary files for setup workflow

--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
 from setuptools import setup, find_packages
+from xharvest import __version__
+
 
 setup(
     name='xharvest',
-    packages=find_packages(),
+    version=__version__,
     url='https://github.abc.com/vyscond/python-xharvest',
     description='Unofficial GTK based Harvest desktop app for Linux',
     long_description=open('README.md').read(),
     install_requires=[
         'pycairo==1.19.1',
         'PyGObject==3.34.0',
+        'keyring==21.2.0',
         'harvest-api==0.2.0',
     ],
+    packages=find_packages(),
     include_package_data=True,
+    package_data={'': ['glade/*.glade']},
+    entry_points={
+        "console_scripts": [
+            "xharvest=xharvest.ui:main"
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
