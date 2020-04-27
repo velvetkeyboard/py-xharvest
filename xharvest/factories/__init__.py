097cb530141a7a3fec941f0b6c2636af18da7d50
Ramon Moraes <ramonmoraes8080@gmail.com>
2020-04-26 22:38:05 -0300

    Removing Factories

--- xharvest/factories/__init__.py
+++
@@ -1,5 +0,0 @@
-from xharvest.factories.weekday import *
-from xharvest.factories.timeentry import *
-from xharvest.factories.timeentryform import *
-from xharvest.factories.settings import *
-from xharvest.factories.authentication import *
