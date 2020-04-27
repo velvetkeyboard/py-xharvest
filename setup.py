e5e7cb64ac5ee2260c01a8ded34bc02255fdb555
Ramon Moraes <ramonmoraes8080@gmail.com>
2020-04-27 14:34:28 -0300

    A lot of formatting adjustments on setup module and bumping version to 0.3.0

--- setup.py
+++ setup.py
@@ -1,40 +1,56 @@
 import os.path
 from setuptools import setup, find_packages
 from xharvest import __version__
+from xharvest import __app_name__
+from xharvest import __author__
+from xharvest import __author_email__
+from xharvest import __license__
 
-HOME_DIR = os.path.expanduser('~')
 
-data_path = f"{HOME_DIR}/.local/share"
-xharvest_cfg_path = f"{HOME_DIR}/.xharvest"
+# -----------------------------------------------------------------------[Meta]
 
 with open('README.md', 'r') as f:
     long_description = f.read()
 
+# ---------------------------------------------------------------------[Assets]
+
+HOME_DIR = os.path.expanduser('~')
+DATA_PATH = f"{HOME_DIR}/.local/share"
+XHARVEST_CFG_PATH = f"{HOME_DIR}/.xharvest"
+
 data_files = [
-    (f'{xharvest_cfg_path}', ['data/rubberduck.jpg']),
-    (f'{data_path}/applications', ['data/org.velvetkeyboad.xHarvest.desktop']),
     (
-        f'{data_path}/icons/hicolor/128x128/apps/',
+        f'{XHARVEST_CFG_PATH}',
+        ['data/rubberduck.jpg'],
+    ),
+    (
+        f'{DATA_PATH}/applications',
+        ['data/org.velvetkeyboad.xHarvest.desktop']),
+    (
+        f'{DATA_PATH}/icons/hicolor/128x128/apps/',
         ['data/hicolor/128x128/xharvest.png']
     ),
     (
-        f'{data_path}/icons/hicolor/48x48/apps/',
+        f'{DATA_PATH}/icons/hicolor/48x48/apps/',
         ['data/hicolor/48x48/xharvest.png']
     ),
     (
-        f'{data_path}/icons/hicolor/32x32/apps/',
+        f'{DATA_PATH}/icons/hicolor/32x32/apps/',
         ['data/hicolor/32x32/xharvest.png']
     ),
     (
-        f'{data_path}/icons/hicolor/16x16/apps/',
+        f'{DATA_PATH}/icons/hicolor/16x16/apps/',
         ['data/hicolor/16x16/xharvest.png']
     ),
 ]
 
 setup(
-    name='xharvest',
+    name=__app_name__,
     version=__version__,
-    url='https://github.com/vyscond/py-xharvest',
+    url='https://github.com/velvetkeyboad/py-xharvest',
+    author=__author__,
+    author_email=__author_email__,
+    license=__license__,
     description='Unofficial Harvest desktop for Linux',
     long_description=long_description,
     install_requires=[
