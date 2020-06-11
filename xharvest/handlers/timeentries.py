dc018b798e560fa5a142a5483ab5e34c0e3ea7ca
Ramon Moraes <ramonmoraes8080@gmail.com>
2020-06-11 20:11:18 -0300

    Handlers - Time Entries - Decoupling fetch data loop

--- xharvest/handlers/timeentries.py
+++ xharvest/handlers/timeentries.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from operator import itemgetter
 from gi.repository import GLib
 from xharvest.threads import GtkThread
+from xharvest.threads import gtk_thread_cb
 from xharvest.utils import remove_all_children
 from xharvest.handlers.base import Handler
 from xharvest.handlers.timeentry import TimeEntryHandler
@@ -11,11 +12,17 @@ class TimeEntriesHandler(Handler):
     template = "time_entries"
     root_widget = "lbox_timeentries"
 
-    PULLING_DELAY = 37
+    PULLING_DELAY = 60 * 2
+
+    def bind_data(self):
+        self.source_remove_id = None
 
     def bind_signals(self):
         self.time_entries.connect("data_update_end", self.render)
         self.week.connect("selected_date_changed", self.render)
+        self.time_entries.connect(
+                "time_entries_were_rendered",
+                self.on_time_entries_were_rendered)
 
     def render(self, gobj):
         lbox = self.get_widget("lbox_timeentries")
@@ -26,10 +33,25 @@ class TimeEntriesHandler(Handler):
             spent_date = datetime.strptime(spent_date, "%Y-%m-%d").date()
             if self.week.get_selected_date().date() == spent_date:
                 lbox.add(TimeEntryHandler(time_entry).get_root_widget())
-        lbox.show_all()
+        # lbox.show_all()
         self.time_entries.emit("time_entries_were_rendered")
+
+
+    def on_time_entries_were_rendered(self, gobj):
+        if self.source_remove_id:
+            GLib.source_remove(self.source_remove_id)
+            self.source_remove_id = None
         self.source_remove_id = GLib.timeout_add_seconds(
             self.PULLING_DELAY,
-            lambda : GtkThread(target=self.time_entries.fetch_data).start(),
+            self.refresh_time_entries,
             )
- 
+
+    def refresh_time_entries(self):
+        self.time_entries.emit('data_update_bgn')
+        GtkThread(
+            target=self.time_entries.fetch_data,
+            target_cb=gtk_thread_cb(
+                lambda t: self.time_entries.emit('data_update_end')
+                ),
+            ).start()
+
