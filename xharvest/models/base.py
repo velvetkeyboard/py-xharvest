1f03288d309e976836e84b029f2020ef471b3c31
Ramon Moraes <ramonmoraes8080@gmail.com>
2020-07-04 12:20:02 -0300

    closes #2
    Adding HarvestGObject GObject. Use it on GObjects that are intent to
    manage Harvest's data

---
+++ xharvest/models/base.py
@@ -0,0 +1,22 @@
+from gi.repository import GObject
+from xharvest.logger import logger
+from xharvest.auth import AuthenticationManager
+
+
+class HarvestGObject(GObject.GObject):
+
+    auth = AuthenticationManager()
+    logger = logger
+
+    def __init__(self):
+        super(HarvestGObject, self).__init__()
+        self.data = None
+
+    def get_credential(self):
+        return self.auth.get_credential()
+
+    def sync_data(self):
+        raise NotImplementedError
+
+    def log(self, *args):
+        print(self.__class__.__name__, *args)
