7a36a32941a6a8e5b9318a83d7fd13dea67439a6
Ramon Moraes <ramonmoraes8080@gmail.com>
2020-07-04 12:12:40 -0300

    Update utilities module
    - migrating get_gravatar_img_as_pixbuf routine to be a stand alone func

--- xharvest/utils.py
+++ xharvest/utils.py
@@ -1,3 +1,9 @@
+import urllib
+import hashlib
+from gi.repository import Gio
+from gi.repository.GdkPixbuf import Pixbuf
+
+
 def find_child_by_name(widget, name):
     if widget.get_name() == name:
         return widget
@@ -11,3 +17,14 @@ def find_child_by_name(widget, name):
 def remove_all_children(widget):
     for c in widget.get_children():
         widget.remove(c)
+
+
+def get_gravatar_img_as_pixbuf(email):
+    size = 40
+    h = hashlib.md5(email.lower().encode("utf-8")).hexdigest()
+    url = f"https://www.gravatar.com/avatar/{h}?d={size}"
+    response = urllib.request.urlopen(url)
+    input_stream = Gio.MemoryInputStream.new_from_data(
+        response.read(), None)
+    pixbuf = Pixbuf.new_from_stream(input_stream, None)
+    return pixbuf
