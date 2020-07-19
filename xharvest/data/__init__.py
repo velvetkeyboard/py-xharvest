735d775b92985d2f4116632c98825ef58e8fb75d
Ramon Moraes <ramonmoraes8080@gmail.com>
2020-07-19 10:26:48 -0300

    Update data module - Removing unused variables

--- xharvest/data/__init__.py
+++ xharvest/data/__init__.py
@@ -1,16 +1,6 @@
 import pkg_resources
 
 
-GLADE_FILE = pkg_resources.resource_filename(__name__, "glade/main2.glade")
-GLADE_MAIN_HEADERBAR = pkg_resources.resource_filename(
-    __name__, "glade/main_headerbar.glade"
-)
-GLADE_TIME_SUMMARY = pkg_resources.resource_filename(
-    __name__, "glade/time_summary.glade"
-)
-CSS_MAIN = pkg_resources.resource_filename(__name__, "css/main.css")
-
-
 def get_template_path(name):
     return pkg_resources.resource_filename(__name__, f"glade/{name}.glade")
 
