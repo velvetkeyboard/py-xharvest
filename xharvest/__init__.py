1ee144bb8da502df41dc90bf1186facbc2457884
Ramon Moraes <ramonmoraes8080@gmail.com>
2020-08-13 15:06:06 -0300

    Add a temporary way to recover "version"

--- xharvest/__init__.py
+++ xharvest/__init__.py
@@ -4,3 +4,7 @@ __author_email__ = "ramonmoraes8080@gmail.com"
 __license__ = "MIT"
 __app_name__ = "xharvest"
 __long_description__ = 'Unofficial Linux GTK based desktop app for Linux.'
+
+
+if __name__ == "__main__":
+    print(__version__)
